43da61e9677e0d8535dcf417d8fe222c2503ef86
Augusto Seta <alseta97@gmail.com>
2023-02-17 15:08:56 -0300

    Added more tests for the blogList app.

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -15,21 +15,82 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 }, 100000)
 
-test('all blogs are returned as json', async () => {
-  const blogs = await api.get('/api/blogs')
-  console.log('BLOGS', blogs);
-
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('HTTP GET /api/blogs', () => {
+  test('all blogs are returned as json', async () => {
+    const blogs = await api.get('/api/blogs')
+  
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
-test('the unique identifier property of blog posts is called id', async () => {
-  const blogs = await api.get('/api/blogs')
-  for (const blog of blogs.body) {
-    expect(blog.id).toBeDefined()
-  }
+describe('HTTP id and not _id', () => {
+  test('the unique identifier property of the blog posts is called id', async () => {
+    const blogs = await api.get('/api/blogs')
+    for (const blog of blogs.body) {
+      expect(blog.id).toBeDefined()
+    }
+  })
+})
+
+describe('HTTP POST /api/blogs', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'test blog',
+      author: 'Augusto Seta',
+      url: '23123123123',
+      likes: 20
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('test blog')
+  })
+})
+
+describe('Check if the likes property exist', () => {
+  test('verifies the likes property', async () => {
+    const responseBlogs = await api.get('/api/blogs')
+    
+    responseBlogs.body.forEach(blog => {
+      if (!blog.likes) {
+        blog.likes = 0
+      }
+    })
+
+    console.log(responseBlogs.body);
+    
+    responseBlogs.body.forEach(blog => {
+      expect(blog.likes).toBeDefined
+    })
+  })
+})
+
+describe('Check if URL or TITLE property is missing', () => {
+  test('Blog has no TITLE or URL properties', async () => {
+    const newBlog = {
+      author: 'Rocko',
+      likes: 2
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsInDb = await helper.blogsInDB()
+    expect(blogsInDb).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(async () => {
